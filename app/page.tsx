61a093b82b53c78b9d0497732918ac96e6e61a38
Sarp <sarp@satato.com>
2024-10-02 12:55:24 +0200

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,89 @@
-import Image from "next/image";
+"use client"
+
+import { avalanche, avalancheFuji } from "wagmi/chains";
+import { echo } from "./chains/definitions/echo";
+
+import { ICTT, PoweredByAvalanche, Web3Provider } from "@0xstt/avalanche-builder-kit";
+import { Info } from 'lucide-react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const chains = [avalanche, avalancheFuji, echo];
+
+  let tokens = [
+    { address: "native", wrapped_address: "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7", name: "Avalanche", symbol: "AVAX", decimals: 18, chain_id: 43114 },
+    {
+      address: "native",
+      wrapped_address: "0xd00ae08403B9bbb9124bB305C09058E32C39A48c",
+      name: "Avalanche",
+      symbol: "AVAX",
+      decimals: 18,
+      chain_id: 43113
+    },
+    // tokens
+    {
+      address: "0x8D6f0E153B1D4Efb46c510278Db3678Bb1Cc823d",
+      name: "TOK",
+      symbol: "TOK",
+      decimals: 18,
+      chain_id: 43113,
+      supports_ictt: true,
+      transferrer: "0xD63c60859e6648b20c38092cCceb92c5751E32fF",
+      mirrors: [
+        {
+          address: "0x8D6f0E153B1D4Efb46c510278Db3678Bb1Cc823d",
+          transferrer: "0x8D6f0E153B1D4Efb46c510278Db3678Bb1Cc823d",
+          chain_id: 173750,
+          decimals: 18
+        }
+      ]
+    },
+    {
+      address: "0x8D6f0E153B1D4Efb46c510278Db3678Bb1Cc823d",
+      name: "TOK",
+      symbol: "TOK",
+      decimals: 18,
+      chain_id: 173750,
+      supports_ictt: true,
+      is_transferrer: true,
+      mirrors: [
+        {
+          address: "0x8D6f0E153B1D4Efb46c510278Db3678Bb1Cc823d",
+          transferrer: "0xD63c60859e6648b20c38092cCceb92c5751E32fF",
+          chain_id: 43113,
+          decimals: 18
+        }
+      ]
+    },
+    { address: "0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7", name: "TetherToken", symbol: "USDt", decimals: 6, chain_id: 43114 },
+    { address: "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7", name: "Wrapped AVAX", symbol: "WAVAX", decimals: 18, chain_id: 43114 },
+    { address: "0xd00ae08403B9bbb9124bB305C09058E32C39A48c", name: "Wrapped AVAX", symbol: "WAVAX", decimals: 18, chain_id: 43113 },
+    { address: "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e", name: "USD Coin", symbol: "USDC", decimals: 6, chain_id: 43114 },
+    { address: "0xd586e7f844cea2f87f50152665bcbc2c279d8d70", name: "Dai Stablecoin", symbol: "DAI.e", decimals: 18, chain_id: 43114 },
+    { address: "0x49d5c2bdffac6ce2bfdb6640f4f80f226bc10bab", name: "Wrapped Ether", symbol: "WETH.e", decimals: 18, chain_id: 43114 },
+    { address: "0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd", name: "JoeToken", symbol: "JOE", decimals: 18, chain_id: 43114 },
+    { address: "0x59414b3089ce2af0010e7523dea7e2b35d776ec7", name: "Yak Token", symbol: "YAK", decimals: 18, chain_id: 43114 },
+    { address: "0x8729438eb15e2c8b576fcc6aecda6a148776c0f5", name: "BENQI", symbol: "QI", decimals: 18, chain_id: 43114 },
+    { address: "0x111111111117dc0aa78b770fa6a738034120c302", name: "1INCH Token", symbol: "1INCH", decimals: 18, chain_id: 1 }
+  ];
+
+  return (
+    <Web3Provider appName="Example Swap App" projectId="YOUR_PROJECT_ID" chains={chains}>
+      <div className="grid grid-cols-12 text-white text-sm items-center">
+        {/* ICTT Example */}
+        <div className="col-span-12 flows-bg w-full h-screen">
+          <div className="flex flex-col w-full h-full justify-center items-center gap-4">
+            <ICTT tokens={tokens} token_in="0x8D6f0E153B1D4Efb46c510278Db3678Bb1Cc823d" source_chain_id={43113} destination_chain_id={173750}></ICTT>
+            <a className="flex items-center gap-2 text-primary-foreground hover:underline" href="https://academy.avax.network/course/interchain-token-transfer" target="_blank">
+              <Info size={16} />
+              <p className="text-xs">What is ICTT?</p>
+            </a>
+            <a className="w-[120px]" href="https://subnets.avax.network/" target="_blank">
+              <PoweredByAvalanche className={"w-full h-full"}></PoweredByAvalanche>
+            </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </Web3Provider>
   );
 }
