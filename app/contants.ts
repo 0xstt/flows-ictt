fb5a7eae9b45c33e81bbc99b68bb0cf485406be8
Sarp <sarp@satato.com>
2024-10-09 16:52:14 +0300

    add faucet

---
+++ app/contants.ts
@@ -0,0 +1,5 @@
+import { fuji } from "./chains/definitions/fuji";
+import { echo } from "./chains/definitions/echo";
+import { dispatch } from "./chains/definitions/dispatch";
+
+export const CHAINS = [ fuji, echo, dispatch ];
--- app/faucet/page.tsx
+++ app/faucet/page.tsx
@@ -1,16 +1,9 @@
 "use client"
 
-import { avalanche } from "wagmi/chains";
-import { fuji } from "./chains/definitions/fuji";
-import { echo } from "./chains/definitions/echo";
-import { dispatch } from "./chains/definitions/dispatch";
-
-import { Faucet, ICTT, PoweredByAvalanche, Web3Provider } from "@0xstt/avalanche-builderkit";
-import { Info } from 'lucide-react';
+import { Faucet, PoweredByAvalanche, Web3Provider } from "@0xstt/avalanche-builderkit";
+import { CHAINS } from "./../contants";
 
 export default function Home() {
-
-  const chains = [avalanche, fuji, echo, dispatch];
 
   const tokens = [
     {
@@ -129,16 +122,13 @@ export default function Home() {
   ];
 
   return (
-    <Web3Provider appName="Flows - ICTT" projectId="YOUR_PROJECT_ID" chains={chains}>
+    <Web3Provider appName="Flows - ICTT" projectId="YOUR_PROJECT_ID" chains={CHAINS}>
       <div className="grid grid-cols-12 text-white text-sm items-center">
         {/* ICTT Example */}
         <div className="col-span-12 flows-bg w-full h-screen">
           <div className="flex flex-col w-full h-full justify-center items-center gap-4">
-            <ICTT tokens={tokens} token_in="0x8D6f0E153B1D4Efb46c510278Db3678Bb1Cc823d" source_chain_id={43113} destination_chain_id={173750}></ICTT>
-            <a className="flex items-center gap-2 text-primary-foreground hover:underline" href="https://academy.avax.network/course/interchain-token-transfer" target="_blank">
-              <Info size={16} />
-              <p className="text-xs">What is ICTT?</p>
-            </a>
+            {/* <ICTT tokens={tokens} token_in="0x8D6f0E153B1D4Efb46c510278Db3678Bb1Cc823d" source_chain_id={43113} destination_chain_id={173750}></ICTT> */}
+            <Faucet tokens={tokens} token_in="0x8D6f0E153B1D4Efb46c510278Db3678Bb1Cc823d" source_chain_id={43113} destination_chain_id={173750}></Faucet>
             <a className="w-[120px]" href="https://subnets.avax.network/" target="_blank">
               <PoweredByAvalanche className={"w-full h-full"}></PoweredByAvalanche>
             </a>
